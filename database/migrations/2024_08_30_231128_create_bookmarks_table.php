e6b00a405f0da611d4e14f1fe1331d0f6c170ad9
rikuto <rikuriku.shin@gmail.com>
2024-08-31 08:31:45 +0900

    feat:Bookmarkモデルの作成リレーション記述とマイグレーションの作成と記述

---
+++ database/migrations/2024_08_30_231128_create_bookmarks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('bookmarks', function (Blueprint $table) {
+            $table->integer('bookmark_id')->autoIncrement();            
+            $table->integer('user_id');
+            $table->integer('post_id');
+            $table->timestamps();
+            
+            $table->foreign('user_id')->references('user_id')->on('users')->onDelete('cascade');
+            $table->foreign('post_id')->references('post_id')->on('posts')->onDelete('cascade');
+            
+            //両カラムの一意性制約
+            $table->unique(['user_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookmarks');
+    }
+};
