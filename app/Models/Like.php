c29d84b404a32a01aa34e6d6d4e596d13743cbc9
rikuto <rikuriku.shin@gmail.com>
2024-08-31 08:50:04 +0900

    feat:Likeモデルの作成とリレーション定義。マイグレーションを作成と記述

---
+++ app/Models/Like.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    use HasFactory;
+
+    protected $table = 'likes';
+
+    protected $primaryKey = 'like_id';
+
+    public $incrementing = true;
+
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+    ];
+
+    public $timestamps = true;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id', 'user_id');
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class, 'post_id', 'post_id');
+    }
+}
