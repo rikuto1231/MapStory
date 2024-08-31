2af2e65f0183d083c6d92b2b3d17bfaa74a005fd
rikuto <rikuriku.shin@gmail.com>
2024-08-31 22:32:40 +0900

    feat:Labelモデルとマイグレーションの作成と構成記述追加

---
+++ database/migrations/2024_08_31_132338_create_labels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('labels', function (Blueprint $table) {
+            $table->integer('label_id')->autoIncrement();
+            $table->integer('post_id');
+            $table->integer('tag_id');
+            $table->timestamp('created_at')->useCurrent();
+
+            $table->foreign('post_id')->references('post_id')->on('posts')->onDelete('cascade');
+            $table->foreign('tag_id')->references('tag_id')->on('tags')->onDelete('cascade');
+
+            $table->unique(['post_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('labels');
+    }
+};
